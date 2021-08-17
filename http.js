dec297603757b7a7dfa0cd21e040098fe59a0576
HAN-IL <stigmah@hanmail.net>
2021-08-17 10:54:14 +0900

    1 file modified

--- http.js
+++ http.js
@@ -3,6 +3,6 @@ var http = require('http');
 http
   .createServer((request, response) => {
     response.writeHead(200, { 'Content-Type': 'text/html' });
-    response.end('<h1>Hello2 World22!</h1>');
+    response.end('<h1>Hello2 World2233!</h1>');
   })
   .listen(52273, () => console.log('Running'));
